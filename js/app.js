db6d601b72927466506477a2601e6013f3cdec6f
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-05-15 16:41:55 -0400

    added carrito functionalities

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const carrito = document.querySelector("#carrito");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 const listaCursos = document.querySelector("#lista-cursos");
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
@@ -33,5 +34,67 @@ function leerDatosCurso(curso) {
         id: curso.querySelector("a").getAttribute("data-id"),
         cantidad: 1,
     }
-    console.log(infoCurso);
+
+    //Revisa si un elemento ya existe en el carrito
+    const existe = articulosCarrito.some( curso => curso.id === infoCurso.id );
+    if(existe) {
+        //actualizamos cantidad
+        const cursos = articulosCarrito.map( curso => {
+            if( curso.id === infoCurso.id ) {
+                curso.cantidad++;
+                return curso;//Objeto actualizado
+            } else {
+                return curso;//objeto no duplicado
+            }
+        });
+        articulosCarrito = [...cursos];
+    } else {
+        //Agregamos el curso al carrito
+        articulosCarrito = [...articulosCarrito, infoCurso];
+    }
+
+    console.log(articulosCarrito);
+
+    carritoHTML();
+}
+
+//Muestra el carrito de compras en el html
+function carritoHTML() {
+
+    //limpiar HTML
+    limpiarHTML();
+
+    //Recorre el HTML del carrito en el tbody
+    articulosCarrito.forEach(curso => {
+
+        //destructuring
+        const { imagen, titulo, precio, cantidad, id } = curso;
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+            <img src="${imagen}" width=100>
+        </img>
+        <td>${titulo}</td>
+        <td>${precio}</td>
+        <td>${cantidad}</td>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${id}"> X </a>
+        </a>
+        
+        ;`
+
+        //Agrega HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+//Elimina los cursos del tbody
+function limpiarHTML() {
+    contenedorCarrito.innerHTML = '';
+
+    //
+    while(contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
 }
